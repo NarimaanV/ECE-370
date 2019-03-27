8dd189cbdd76a70c6487537c34aa94b90aa9f5c0
Narimaan Valian <Narimaanvalian@yahoo.com>
2019-03-27 02:09:47 -0400

    Added code to drive H-bridge from previous homework

--- servo_arduino/servo_arduino.ino
+++ servo_arduino/servo_arduino.ino
@@ -1,9 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
+#define MOTOR_A 12
+#define MOTOR_B 11
 
+int set_speed(float s);
+
+void set_velocity(float v);
+
+void setup()
+{
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  
+}
 
+// Maps s to corresponding int value on 0-255 scale (clamps values bigger/smaller than 1.0/0.0 to 1.0/0.0, respectively).
+int set_speed(float s)
+{
+  if (s < 0.0f)
+    s = 0.0f;
+  else if (s > 1.0f)
+    s = 1.0f;
+  return (int)(s * 255.0f);
+}
+
+// Outputs PWM on either MOTOR_A or MOTOR_B pins depending on sign
+void set_velocity(float v)
+{
+  if (v > 0.0f)
+  {
+    analogWrite(MOTOR_A, set_speed(v));
+    analogWrite(MOTOR_B, 0);
+  }
+
+  else if (v < 0.0f)
+  {
+    analogWrite(MOTOR_A, 0);
+    analogWrite(MOTOR_B, set_speed(-1.0f * v));
+  }
+
+  else
+  {
+    analogWrite(MOTOR_A, 0);
+    analogWrite(MOTOR_B, 0);
+  }
 }
