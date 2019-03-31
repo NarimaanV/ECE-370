08a7f571b03f12d29178a150efd5d6e3c82f13b1
Narimaan Valian <Narimaanvalian@yahoo.com>
2019-03-31 01:25:12 -0400

    Greatly simplfied P-controller code, still must fix instability

--- servo_arduino/servo_arduino.ino
+++ servo_arduino/servo_arduino.ino
@@ -12,11 +12,15 @@
  */
 const double ticks_per_degree = (8.0 * 75.81) / 360.0,  // 0.8423
              degrees_per_tick = 360.0 / (8.0 * 75.81),  // 1.1872
-             K_p = 0.1;
+             K_p = -1;
 
-long desired_angle, angle, desired_ticks, ticks = 0, error, cur_speed;
-unsigned long tick_time, tock_time, desired_control_time = 50000;
+long  desired_ticks, ticks = 0, error, cur_speed;
+unsigned long tick_time, tock_time, desired_control_time = 50000, i_wait, i_wait2 = 0, angle_pos = 0;
 short new_input = 1;
+
+short angles[3] = {90.0, 135.0, 0.0};
+
+float desired_angle = 0.0, angle;
 
 int set_speed(float s);
 void set_velocity(float v);
@@ -37,7 +41,7 @@ struct state
 
 void setup()
 {
-  Serial.begin(9600);
+//  Serial.begin(9600);
   pinMode(MOTOR_A, OUTPUT);
   pinMode(MOTOR_B, OUTPUT);
   pinMode(IR_A, INPUT);
@@ -55,52 +59,55 @@ void loop()
     // Get time at beginning of P controller loop
     tick_time = micros();
 
-    // Wait for angle to be sent via serial
-    if (new_input)
+//    if (Serial.available())
+//      desired_angle = Serial.parseFloat();
+    i_wait2++;
+    if (i_wait2 == 200)
     {
-      if (Serial.available())
-      {
-        desired_angle = Serial.parseInt();
-        desired_ticks = (int)((double)desired_angle * ticks_per_degree);
-        if (desired_ticks < 0)
-          desired_ticks *= -1;
-        if (desired_angle > 0)
-          dir_cur = CCW;
-        else
-          dir_cur = CW;
-        new_input = 0;
-      }
+      i_wait2 = 0;
+      desired_angle = angles[angle_pos % 3];
+      angle_pos++;
     }
 
-    // Actually try getting to that angle
-    else
-    {
-      error = desired_ticks - ticks;
-      
-      cur_speed = error * K_p * dir_cur;
-      set_velocity(cur_speed);
-      if (error < 3)
-        new_input = 1;
-    }
+    angle = ticks * degrees_per_tick;
+    
+//    set_velocity(0.0);
 
-    Serial.println(ticks * degrees_per_tick);
-    set_velocity(1.0);
+    float e = desired_angle - angle;
+    float c = K_p * e;
+    
+    set_velocity(c);
+    i_wait++;
+//    if (i_wait == 10)
+//    {
+//      i_wait = 0;
+//      Serial.print(angle);
+//      Serial.print(" ");
+//      Serial.print(c);
+//      Serial.println();
+//    }
+//    Serial.print(angle);
+//    Serial.print(" ");
+//    Serial.print(c);
+//    Serial.println();
 
     // Get time at end of P controller loop
     tock_time = micros();
 
     // Ensure at least the minimum delay has occured for P controller loop
 //    delayMicroseconds(desired_control_time - (tock_time - tick_time));
+    delay(50);
   }
 }
 
 // Maps s to corresponding int value on 0-255 scale (clamps values bigger/smaller than 1.0/0.5 to 1.0/0.5, respectively).
 int set_speed(float s)
 {
-  if (s < 0.5f)
-    s = 0.5f;
+  if (s < 0.0f)
+    s = 0.0f;
   else if (s > 1.0f)
     s = 1.0f;
+//  Serial.println((int)(s * 255.0f));
   return (int)(s * 255.0f);
 }
 
