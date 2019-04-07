f400ba24046dfa9be4980869281609aee6be2644
Narimaan Valian <Narimaanvalian@yahoo.com>
2019-04-07 00:32:22 -0400

    Added constants for pin numbers and dimensions, added ISR skeletons for each wheel with each method, and added setup code for pin directions and ISR assignment.

--- odometry/odometry.ino
+++ odometry/odometry.ino
@@ -1,9 +1,57 @@
+// Constant values for pin numbers and robot dimensions
+#define RADIUS      30  // Wheel radii in mm
+#define BASELINE    80  // Transaxial distance between center of each wheels
+#define MOTOR_RIGHT 12  // Right motor pin
+#define MOTOR_LEFT  11  // Left motor pin
+#define IR_RIGHT    6   // Right wheel IR sensor pin
+#define IR_LEFT     5   // Left wheel IR sensor pin
+
+// Preprocessor flag for switching between analytical and matrix methods of odometry
+#define MATRIX_METHOD
+
 void setup()
+{
+  // Set pin directions for motors and IR sensors
+  pinMode(MOTOR_RIGHT, OUTPUT);
+  pinMode(MOTOR_LEFT, OUTPUT);
+  pinMode(IR_RIGHT, INPUT);
+  pinMode(IR_LEFT, INPUT);
+
+  // Choose ISRs for certain method dependong in preprocessor flag
+  #ifdef MATRIX_METHOD
+    attachInterrupt(digitalPinToInterrupt(IR_RIGHT), matrix_odometry_right, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(IR_LEFT), matrix_odometry_left, CHANGE);
+  #else
+    attachInterrupt(digitalPinToInterrupt(IR_RIGHT), analytical_odometry_right, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(IR_LEFT), analytical_odometry_left, CHANGE);
+  #endif
+}
+
+void loop()
+{
+  // Do robot stuff
+}
+
+// ISR for calculating odometry of right wheel based on IR sensor signal using analytical method
+void analytical_odometry_right()
 {
   
 }
 
-void loop()
+// ISR for calculating odometry of left wheel based on IR sensor signal using analytical method
+void analytical_odometry_left()
+{
+  
+}
+
+// ISR for calculating odometry of right wheel based on IR sensor signal using matrix method
+void matrix_odometry_right()
+{
+  
+}
+
+// ISR for calculating odometry of left wheel based on IR sensor signal using matrix method
+void matrix_odometry_left()
 {
   
 }
