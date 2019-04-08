750ff21381a602e66898662b85d409393dd9e687
Narimaan Valian <Narimaanvalian@yahoo.com>
2019-04-08 01:35:19 -0400

    Added conversion from ticks to phi radians and gloabsl for analytical method

--- odometry/odometry.ino
+++ odometry/odometry.ino
@@ -1,12 +1,14 @@
 #include <BasicLinearAlgebra.h>
 
 // Constant values for pin numbers and robot dimensions
-#define RADIUS      30  // Wheel radii in mm
-#define BASELINE    80  // Transaxial distance between center of each wheels
-#define MOTOR_RIGHT 12  // Right motor pin
-#define MOTOR_LEFT  11  // Left motor pin
-#define IR_RIGHT    6   // Right wheel IR sensor pin
-#define IR_LEFT     5   // Left wheel IR sensor pin
+#define RADIUS          30    // Wheel radii in mm
+#define BASELINE        80    // Transaxial distance between center of each wheels
+#define MOTOR_RIGHT     12    // Right motor pin
+#define MOTOR_LEFT      11    // Left motor pin
+#define IR_RIGHT        6     // Right wheel IR sensor pin
+#define IR_LEFT         5     // Left wheel IR sensor pin
+#define GEAR_RATIO      75.81 // Gear ratio of motor gearbox (input/output)
+#define TICKS_PER_ROT   2     // How many ticks will register per rotation of encoder wheel
 
 // Preprocessor flag for switching between analytical and matrix methods of odometry
 #define MATRIX_METHOD
@@ -14,7 +16,21 @@
 // Since only one library for matrices is being used, this eliminates needing to preface all matrix-related operations with "BLA::"
 using namespace BLA;
 
-Matrix<4, 4, Array<4, 4, double>> T_right, T_right_prev, T_left, T_left_prev;
+// Transformation matrices for right and left weheel
+Matrix<4, 4, Array<4, 4, double>> T_right, T_left;
+
+// Current global-referenced values for x, y, and phi (all start at 0);
+double x_global = 0.0,
+       y_global = 0.0,
+       phi_global = 0.0;
+
+// Phi angle radians per single tick
+// (RADIUS/BASELINE) phi radians/wheel radians 
+// 2pi wheel radians/wheel rotation
+// 1/GEAR_RATIO wheel rotations/encoder rotation
+// 1/TICKS_PER_ROT encoder rotation/tick
+// So phi radians / tick = ((RADIUS/BASELINE) phi radians/wheel radians)*(2pi wheel radians/wheel rotation)*(1/GEAR_RATIO wheel rotations/encoder rotation)*(1/TICKS_PER_ROT encoder rotation/tick)
+const double phi_radians_per_tick = (RADIUS / BASELINE) * (2.0 * PI) * (1.0 / 75.81) * (1.0 / TICKS_PER_ROT);
 
 void setup()
 {
@@ -42,7 +58,7 @@ void loop()
 // ISR for calculating odometry of right wheel based on IR sensor signal using analytical method
 void analytical_odometry_right()
 {
-  
+   
 }
 
 // ISR for calculating odometry of left wheel based on IR sensor signal using analytical method
