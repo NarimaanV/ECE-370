d3129c96739a83780d335288abd1b007df2f5614
Narimaan Valian <Narimaanvalian@yahoo.com>
2019-04-27 16:18:16 -0400

    Added PyGame functionality to get arrow key inputs easily

--- UDP_Test_Send_Receive_Python.py
+++ UDP_Test_Send_Receive_Python.py
@@ -9,7 +9,12 @@ translate = 0.0
 rotate = 0.0
 mode = 0
 
+x = 0.0
+y = 0.0
+phi = 0.0
+
 pygame.init()
+screen = pygame.display.set_mode((1,1))
 sock = socket(AF_INET, SOCK_DGRAM) # Internet, UDP
 
 def sendCommand(translate, rotate, mode):
@@ -21,30 +26,38 @@ def sendCommand(translate, rotate, mode):
 
     return
 
-while True:
-    # Store 3 inputs from command line (float float int)
-    #translate, rotate, mode = input().split(' ')
-    
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                translate += 10.0
-            if event.key == pygame.K_DOWN and translate >= 10.0:
-                translate -= 10.0
-            if event.key == pygame.K_LEFT:
-                rotate += 10.0
-            if event.key == pygame.K_RIGHT:
-                rotate -= 10.0
-            if event.key == pygame.K_q:
-                print("Current X: {}, Current Y: {}, Current Phi: {}")
-            send_command(translate, rotate, mode)
-
+def recvInfo():
     # Wait to recieve current position information struct from UDP socket/port
     info, addr = sock.recvfrom(12)
 
     # Unpack current position information struct into 3 individual floats
     x, y, phi = unpack("=fff", info)
 
-    # Print current position information
-    print(str(x) + ", " + str(y) + ", " + str(phi))
+    return
+
+while True:
+    # Store 3 inputs from command line (float float int)
+    #translate, rotate, mode = input().split(' ')
+    
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                translate += 10.0
+                sendCommand(translate, rotate, mode)
+                recvInfo()
+            if event.key == pygame.K_DOWN and translate >= 10.0:
+                translate -= 10.0
+                sendCommand(translate, rotate, mode)
+                recvInfo()
+            if event.key == pygame.K_LEFT:
+                rotate += 10.0
+                sendCommand(translate, rotate, mode)
+                recvInfo()
+            if event.key == pygame.K_RIGHT:
+                rotate -= 10.0
+                sendCommand(translate, rotate, mode)
+                recvInfo()
+            if event.key == pygame.K_q:
+                print("Current X: {}, Current Y: {}, Current Phi: {}".format(x,
+                    y, phi))
+    pygame.event.pump()
