5006c4ae04314674cf6b690442a3f5a81fb059bd
Narimaan Valian <Narimaanvalian@yahoo.com>
2019-04-28 14:28:59 -0400

    Fixed several issues with odometry measurements

--- odometry/odometry.ino
+++ odometry/odometry.ino
@@ -1,10 +1,10 @@
 #include <BasicLinearAlgebra.h>
 
 // Constant values for pin numbers and robot dimensions
-#define RADIUS          31.4  // Wheel radii in mm
-#define BASELINE        82.4  // Transaxial distance between center of each wheels
-#define GEAR_RATIO      80 // Gear ratio of motor gearbox (input/output) 75.81
-#define TICKS_PER_ROT   2     // How many ticks will register per rotation of encoder wheel (depends on both encoder design AND what edge(s) trigger interrupts)
+#define RADIUS          15.7f // Wheel radii in mm 31.4/2
+#define BASELINE        82.4f // Transaxial distance between center of each wheels 82.4
+#define GEAR_RATIO      75.81f // Gear ratio of motor gearbox (input/output) 75.81
+#define TICKS_PER_ROT   2.0f  // How many ticks will register per rotation of encoder wheel (depends on both encoder design AND what edge(s) trigger interrupts)
 #define MOTOR_RIGHT     12    // Right motor pin
 #define MOTOR_LEFT      11    // Left motor pin
 #define IR_RIGHT        6     // Right wheel IR sensor pin
@@ -17,9 +17,9 @@
 using namespace BLA;
 
 // Current global-referenced values for x, y, and phi (all start at 0.0);
-float x_global = 0.0,
-       y_global = 0.0,
-       phi_global = 0.0;
+float x_global = 0.0f,
+       y_global = 0.0f,
+       phi_global = 0.0f;
 
 // Phi angle radians per single tick
 // (RADIUS/BASELINE) phi radians/wheel radians 
@@ -27,13 +27,13 @@ float x_global = 0.0,
 // 1/GEAR_RATIO wheel rotations/encoder rotation
 // 1/TICKS_PER_ROT encoder rotation/tick
 // So phi radians / tick = ((RADIUS/BASELINE) phi radians/wheel radians)*(2pi wheel radians/wheel rotation)*(1/GEAR_RATIO wheel rotations/encoder rotation)*(1/TICKS_PER_ROT encoder rotation/tick)
-const float phi_radians_per_tick = (RADIUS / BASELINE) * (2.0 * PI) * (1.0 / 75.81) * (1.0 / TICKS_PER_ROT);
+const float phi_radians_per_tick = (RADIUS / BASELINE) * (2.0f * PI) * (1.0f / 75.81f) * (1.0f / TICKS_PER_ROT);
 
 // Transformation matrices 
-Matrix<4, 4, Array<4, 4, float>> T = {1.0, 0.0, 0.0, 0.0, // Current transformation matrix between global origin and robot center (identity matrix since initial position is assumed to be x, y, phi = 0.0, 0.0, 0.0)
-                                      0.0, 1.0, 0.0, 0.0,
-                                      0.0, 0.0, 1.0, 0.0,
-                                      0.0, 0.0, 0.0, 1.0},
+Matrix<4, 4> T = {1.0, 0.0, 0.0, 0.0, // Current transformation matrix between global origin and robot center (identity matrix since initial position is assumed to be x, y, phi = 0.0, 0.0, 0.0)
+                  0.0, 1.0, 0.0, 0.0,
+                  0.0, 0.0, 1.0, 0.0,
+                  0.0, 0.0, 0.0, 1.0},
                                             
                                   translate_right = {1.0, 0.0, 0.0, 0.0, // Right wheel translational transformation matrix (never changes)
                                                      0.0, 1.0, 0.0, -BASELINE,
@@ -46,12 +46,12 @@ Matrix<4, 4, Array<4, 4, float>> T = {1.0, 0.0, 0.0, 0.0, // Current transformat
                                                     0.0, 0.0, 0.0, 1.0},
 
                                   rotate_right = {cos(phi_radians_per_tick), -sin(phi_radians_per_tick), 0.0, 0.0,  // Right wheel rotational transformation matrix (never changes)
-                                                  sin(phi_radians_per_tick), cos(phi_radians_per_tick),  0.0, BASELINE / 2.0,
+                                                  sin(phi_radians_per_tick), cos(phi_radians_per_tick),  0.0, 0.0,
                                                   0.0,                       0.0,                        1.0, 0.0,
                                                   0.0,                       0.0,                        0.0, 1.0},
                                                     
                                   rotate_left = {cos(-phi_radians_per_tick), -sin(-phi_radians_per_tick), 0.0, 0.0,   // Left wheel rotational transformation matrix (never changes)
-                                                 sin(-phi_radians_per_tick), cos(-phi_radians_per_tick),  0.0, -BASELINE / 2.0,
+                                                 sin(-phi_radians_per_tick), cos(-phi_radians_per_tick),  0.0, 0.0,
                                                  0.0,                        0.0,                        1.0, 0.0,
                                                  0.0,                        0.0,                        0.0, 1.0};
 
@@ -73,37 +73,45 @@ void setup()
     attachInterrupt(digitalPinToInterrupt(IR_LEFT), analytical_odometry_left, RISING);
   #endif  // MATRIX_METHOD
 
-  delay(5000);
-  char out[50];
-  
-  Serial.print("Initial: X = ");
-  Serial.print(x_global);
-  Serial.print(", Y = ");
-  Serial.print(y_global);
-  Serial.print(", Phi = ");
-  Serial.println(phi_global);
-  for (int i = 0; i < 160; i++)
-  {
-    analytical_odometry_right();
-    analytical_odometry_left();
-  }
-  Serial.print("Final: X = ");
-  Serial.print(x_global);
-  Serial.print(", Y = ");
-  Serial.print(y_global);
-  Serial.print(", Phi = ");
-  Serial.println(phi_global);
+//  delay(5000);
+//  char out[50];
+//  
+//  Serial.print("\nMatrix Initial: X = ");
+//  Serial.print(x_global, 5);
+//  Serial.print(", Y = ");
+//  Serial.print(y_global, 5);
+//  Serial.print(", Phi = ");
+//  Serial.println(phi_global, 5);
+//  for (int i = 0; i < 80; i++)
+//  {
+//    matrix_odometry_right();
+////    matrix_odometry_left();
+//  }
+//  Serial.print("Matrix Final: X = ");
+//  Serial.print(x_global, 5);
+//  Serial.print(", Y = ");
+//  Serial.print(y_global, 5);
+//  Serial.print(", Phi = ");
+//  Serial.println(phi_global, 5);
 }
 
 void loop()
 {
-  // Do robot stuff, I bet it'll look cool
+  Serial.print("Current: X = ");
+  Serial.print(x_global, 5);
+  Serial.print(", Y = ");
+  Serial.print(y_global, 5);
+  Serial.print(", Phi = ");
+  Serial.println(phi_global, 5);
+
+  delay(500);
 }
 
 // ISR for calculating odometry of right wheel based on IR sensor signal using analytical method
 void analytical_odometry_right()
 {
   float delta_x, delta_y;  // Local delta x and delta y values
+  float temp_x, temp_y;
   
   phi_global += phi_radians_per_tick; // Calculate new global phi angle based on ratio between phi radians and ticks
   
