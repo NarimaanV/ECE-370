23f94fb6a914c3e2c9979765ee38d4e86f81d594
Narimaan Valian <Narimaanvalian@yahoo.com>
2019-05-14 18:44:47 -0400

    Established two-way UDP communication using Python code and have odometry mostly working at the moment

--- Project_3_M0/Project_3_M0.ino
+++ Project_3_M0/Project_3_M0.ino
@@ -6,9 +6,9 @@
 #include <math.h>
 
 // Constant values for pin numbers and robot dimensions
-#define RADIUS          58.4f // Wheel radii in mm 31.4/2
-#define BASELINE        87.5f // Transaxial distance between center of each wheels 82.4
-#define GEAR_RATIO      75.81f // Gear ratio of motor gearbox (input/output) 75.81
+#define RADIUS          29.0 // Wheel radii in mm 31.4/2
+#define BASELINE        87.5 // Transaxial distance between center of each wheels 82.4
+#define GEAR_RATIO      75.81 // Gear ratio of motor gearbox (input/output) 75.81
 #define TICKS_PER_ROT   2.0f  // How many ticks will register per rotation of encoder wheel (depends on both encoder design AND what edge(s) trigger interrupts)
 #define MOTOR_RIGHT_FORWARD   12    // Right motor pin
 #define MOTOR_RIGHT_REVERSE   11    // Right motor pin
@@ -29,14 +29,14 @@ struct __attribute__((__packed__)) command
   double translational;
   double angle;
   int mode;
-} input_command;
+} input_command = {0.0, 0.0, 0};
 
 struct __attribute__((__packed__)) robot_info
 {
   double odo[3];
   double imu[6];
   double head;
-} cur_info = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0, 7.0, 8.0, 9.0}, 10.0};
+} cur_info = {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0, 0.0, 0.0, 0.0}, 0.0};
 
 //char ssid[] = "Narimaan-M0";
 int status = WL_IDLE_STATUS;
@@ -61,7 +61,7 @@ unsigned long tick_time, tock_time, desired_control_time = 10;
 // 1/GEAR_RATIO wheel rotations/encoder rotation
 // 1/TICKS_PER_ROT encoder rotation/tick
 // So phi radians / tick = ((RADIUS/BASELINE) phi radians/wheel radians)*(2pi wheel radians/wheel rotation)*(1/GEAR_RATIO wheel rotations/encoder rotation)*(1/TICKS_PER_ROT encoder rotation/tick)
-const float phi_radians_per_tick = (RADIUS / BASELINE) * (2.0f * PI) * (1.0f / GEAR_RATIO) * (1.0f / TICKS_PER_ROT);
+const float phi_radians_per_tick = (RADIUS / BASELINE) * (PI) * (1.0f / GEAR_RATIO) * (1.0f / TICKS_PER_ROT);
 
 // Constant delta_x and delta_y values based on phi_radians_per_tick used by ISRs
 const float delta_x = (BASELINE / 2.0) * sin(phi_radians_per_tick); // Calculate local delta x based on local phi
@@ -77,17 +77,20 @@ LSM303 compass;
 void setup()
 {
   Serial.begin(9600);
-  while(!Serial);
+
   pinMode(MOTOR_RIGHT_FORWARD, OUTPUT);
   pinMode(MOTOR_RIGHT_REVERSE, OUTPUT);
   pinMode(MOTOR_LEFT_FORWARD, OUTPUT);
   pinMode(MOTOR_LEFT_REVERSE, OUTPUT);
+  pinMode(IR_RIGHT, INPUT_PULLUP);
+  pinMode(IR_LEFT, INPUT_PULLUP);
   analogWrite(MOTOR_RIGHT_FORWARD, 0);
   analogWrite(MOTOR_RIGHT_REVERSE, 0);
   analogWrite(MOTOR_LEFT_FORWARD, 0);
   analogWrite(MOTOR_LEFT_REVERSE, 0);
   attachInterrupt(digitalPinToInterrupt(IR_RIGHT), analytical_odometry_right, RISING);
   attachInterrupt(digitalPinToInterrupt(IR_LEFT), analytical_odometry_left, RISING);
+//  while(!Serial);
   Wire.begin();
   compass.init();
   compass.enableDefault();
@@ -108,73 +111,41 @@ void setup()
   printWiFiData();
   
   send_udp.begin(send_port);
-  receive_udp.begin(receive_port);
+//  receive_udp.begin(receive_port);
 
   info_time = millis();
-  compass.read();
-  start_angle = compass.heading((LSM303::vector<int>){-1, 0, 0});
+  cur_info.odo[0] = 0.0;
+  cur_info.odo[1] = 0.0;
+  cur_info.odo[2] = 0.0;
+//  compass.read();
+//  start_angle = compass.heading((LSM303::vector<int>){-1, 0, 0});
 }
 
 void loop()
 {
   tick_time = millis();
   
-  if (packetSize = receive_udp.parsePacket())
-    receive_udp.read((char*)(&input_command), sizeof(command));
-
-  if (millis() - info_time >= 1000)
+  if (packetSize = send_udp.parsePacket())
   {
-    send_udp.beginPacket(pi, send_port);
-    char send_buffer[1024] = { 0 };
-    memcpy(send_buffer, &cur_info, sizeof(robot_info));
-    send_udp.write(send_buffer, sizeof(robot_info));
+    send_udp.read((char*)(&input_command), sizeof(command));
+    send_udp.beginPacket(send_udp.remoteIP(), send_udp.remotePort());
+    send_udp.write((char*)&cur_info, sizeof(cur_info));
     send_udp.endPacket();
+    Serial.println(input_command.translational);
+    Serial.println(input_command.angle);
+    Serial.println(input_command.mode);
     Serial.println("Sent!");
     info_time = millis();
   }
-
-  compass.read();
-  cur_info.odo[2] = compass.heading((LSM303::vector<int>){-1, 0, 0});
+  
+//  compass.read();
+//  cur_info.odo[2] = compass.heading((LSM303::vector<int>){-1, 0, 0});
   acc_y = (float)compass.a.y * 0.061 / 1000.0;
 
   if (acc_y > 1.15f)
   {
     rotate(0.0);
     reset = 1;
-  }
-
-  switch (input_command.mode)
-  {
-    case 0:
-      K_p = 3.0;
-      desired_angle = start_angle + input_command.angle;
-      if (desired_angle > 360.0f)
-        desired_angle -= 360.0f;
-      else if (desired_angle < 0.0f)
-        desired_angle += 360.0f;
-      break;
-    case 1:
-      K_p = 2.0;
-      start_angle = desired_angle = 0.0f;
-      input_command.mode = 5;
-      break;
-    case 2:
-      K_p = 2.0;
-      start_angle = desired_angle = 270.0f;
-      input_command.mode = 5;
-      break;
-    case 3:
-      K_p = 2.0;
-      start_angle = desired_angle = 180.0f;
-      input_command.mode = 5;
-      break;
-    case 4:
-      K_p = 2.0;
-      start_angle = desired_angle = 90.0f;
-      input_command.mode = 5;
-      break;
-    default:
-      break;
   }
 
   if (!reset)
@@ -291,15 +262,19 @@ void printMacAddress(byte mac[]) {
 // ISR for calculating odometry of right wheel based on IR sensor signal using analytical method
 void analytical_odometry_right()
 { 
+  cur_info.odo[2] *= (PI / 180.0);
   cur_info.odo[2] += phi_radians_per_tick; // Calculate new global phi angle based on ratio between phi radians and ticks
   cur_info.odo[0] += (delta_x * cos(cur_info.odo[2])) + (delta_y * sin(cur_info.odo[2]));  // Update global x based on global phi and local delta x and y
   cur_info.odo[1] += (delta_x * sin(cur_info.odo[2])) + (delta_y * cos(cur_info.odo[2]));  // Update global y based on global phi and local delta x and y
+  cur_info.odo[2] *= (180.0 / PI);
 }
 
 // ISR for calculating odometry of left wheel based on IR sensor signal using analytical method
 void analytical_odometry_left()
 {
+  cur_info.odo[2] *= (PI / 180.0);
   cur_info.odo[2] -= phi_radians_per_tick; // Calculate new global phi angle based on ratio between phi radians and ticks
   cur_info.odo[0] += (delta_x * cos(cur_info.odo[2])) + (delta_y * sin(cur_info.odo[2]));  // Update global x based on global phi and local delta x and y
   cur_info.odo[1] -= (delta_x * sin(cur_info.odo[2])) + (delta_y * cos(cur_info.odo[2]));  // Update global y based on global phi and local delta x and y
+  cur_info.odo[2] *= (180.0 / PI);
 }
--- Project_3_Pi.py
+++ Project_3_Pi.py
@@ -1,40 +1,68 @@
 import socket
-from ctypes import *
+from struct import *
+from getkey import getkey, keys
 
 UDP_IP = "192.168.1.74"
-UDP_SEND_PORT = 5005
 UDP_REC_PORT = 4242
 
-send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, UDP
 rec_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, UDP
 
-send_sock.connect((UDP_IP, UDP_SEND_PORT))
 rec_sock.connect((UDP_IP, UDP_REC_PORT))
 
-class Command(Structure):
-    _pack_ = 1
-    _fields_ = [
-            ("translational", c_double),
-            ("angle", c_double),
-            ("mode", c_int)
-            ]
+translational = 0.0
+angle = 0.0
+mode = 0
 
-class Robot_Info(Structure):
-    _pack_ = 1
-    _fields_ = [
-            ("odo", c_double * 3),
-            ("imu", c_double * 6),
-            ("head", c_double)
-            ]
+odo = [0.0, 0.0, 0.0, 0.0]
+imu = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+head = 0.0
 
-input_command = None
-cur_info = None
+print_info = False
 
 while True:
-    input_buffer, addr = rec_sock.recvfrom(sizeof(Robot_Info))
-    cur_info = Robot_Info.from_buffer_copy(input_buffer)
-    print("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(cur_info.odo[0],
-        cur_info.odo[1], cur_info.odo[2], cur_info.imu[0], cur_info.imu[1],
-        cur_info.imu[2], cur_info.imu[3], cur_info.imu[4], cur_info.imu[5],
-        cur_info.head))
-    #print("received!")
+    key = getkey()
+
+    if key == keys.UP:
+        if translational == 0.0:
+            translational = 50.0
+        else:
+            translational += 25.0
+    elif key == keys.DOWN:
+        if translational <= 50.0:
+            translational = 0.0
+        else:
+            translational -= 25.0
+    elif key == keys.LEFT:
+        angle -= 15.0
+    elif key == keys.RIGHT:
+        angle += 15.0
+    elif key == keys.SPACE:
+        translational = 0.0
+    elif key == 'w':
+        angle = 0.0
+    elif key == 'a':
+        angle = 270.0
+    elif key == 's':
+        angle = 180.0
+    elif key == 'd':
+        angle = 90.0
+    elif key == 'q':
+        print_info = True
+    elif key == 'r':
+        pass
+    else:
+        pass
+
+    rec_sock.send(pack("=ddi", translational, angle, mode))
+    input_buffer = rec_sock.recv(80)
+    (odo[0], odo[1], odo[2], imu[0], imu[1], imu[2], imu[3], imu[4], imu[5],
+            head) = unpack("=dddddddddd", input_buffer)
+    if print_info == True:
+        print "Odometry: x = {}, y = {}, phi = {}".format(odo[0], odo[1],
+                odo[2])
+        print "IMU Acceleration: x = {} g, y = {} g, z = {} g".format(imu[0],
+                imu[1], imu[2])
+        print "IMU Magnetism: x = {} gauss, y = {} gauss, z = {} gauss".format(imu[3],
+                imu[4], imu[5])
+        print "Heading: {} degrees".format(head)
+        print_info = False
