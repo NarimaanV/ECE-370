67036bc5440219ea036167fbffc2431eff55e722
Narimaan Valian <Narimaanvalian@yahoo.com>
2019-05-14 22:11:06 -0400

    Added Task_1_Odometry.py

---
+++ task1-odometry/Task_1_Odometry.py
@@ -0,0 +1,52 @@
+import socket
+from struct import *
+from getkey import getkey, keys
+
+UDP_IP = "192.168.1.74"
+UDP_PORT = 4242
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Internet, UDP
+
+sock.connect((UDP_IP, UDP_PORT))
+
+translational = 0.0
+rotational = 0.0
+mode = 0
+
+x = 0.0
+y = 0.0
+phi = 0.0
+
+print_info = False
+
+while True:
+    key = getkey()
+
+    if key == keys.UP:
+        if translational == 0.0:
+            translational = 2000.0
+        else:
+            translational += 1000.0
+    elif key == keys.DOWN:
+        if translational <= 2000.0:
+            translational = 0.0
+        else:
+            translational -= 1000.0
+    elif key == keys.LEFT:
+        rotational -= 15.0
+    elif key == keys.RIGHT:
+        rotational += 15.0
+    elif key == 'q':
+        print_info = True
+    else:
+        pass
+
+    sock.send(pack("=ddi", translational, rotational, mode))
+    input_buffer = sock.recv(24)
+    (x, y, phi) = unpack("=ddd", input_buffer)
+
+    if print_info == True:
+        print "Odometry: x = {} mm, y = {} mm, phi = {} degrees".format(x, y,
+                phi)
+        print_info = False
+
