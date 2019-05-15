17b4cacf542801ffd8a0d17acac2ee7644d8eebb
Narimaan Valian <Narimaanvalian@yahoo.com>
2019-05-14 22:09:18 -0400

    Updated code to enable two-way UDP communication using Python instead of C

--- task1-odometry/Task_1_Odometry_M0/Task_1_Odometry_M0.ino
+++ task1-odometry/Task_1_Odometry_M0/Task_1_Odometry_M0.ino
@@ -4,16 +4,16 @@
 #include <math.h>
 
 // Constant values for pin numbers and robot dimensions
-#define RADIUS          15.7f // Wheel radii in mm 31.4/2
-#define BASELINE        82.4f // Transaxial distance between center of each wheels 82.4
+#define RADIUS          29.0 // Wheel radii in mm 31.4/2
+#define BASELINE        87.5 // Transaxial distance between center of each wheels 82.4
 #define GEAR_RATIO      75.81f // Gear ratio of motor gearbox (input/output) 75.81
 #define TICKS_PER_ROT   2.0f  // How many ticks will register per rotation of encoder wheel (depends on both encoder design AND what edge(s) trigger interrupts)
 #define MOTOR_RIGHT_A   12    // Right motor pin
 #define MOTOR_RIGHT_B   11    // Right motor pin
 #define MOTOR_LEFT_A    10    // Left motor pin
 #define MOTOR_LEFT_B    9    // Left motor pin
-#define IR_RIGHT        6     // Right wheel IR sensor pin
-#define IR_LEFT         5     // Left wheel IR sensor pin
+#define IR_RIGHT        5     // Right wheel IR sensor pin
+#define IR_LEFT         6     // Left wheel IR sensor pin
 
 // Network constants
 #define SECRET_SSID "Unifi-Home"
@@ -21,17 +21,17 @@
 
 struct __attribute__((__packed__)) command
 {
-  float translational;
-  float rotational;
+  double translational;
+  double rotational;
   int mode;
-} input_command;
+} input_command = {0.0, 0.0, 0};
 
 struct __attribute__((__packed__)) robot_info
 {
-  float x;
-  float y;
-  float phi;
-} cur_info = {0.0f, 0.0f, 0.0f};
+  double x;
+  double y;
+  double phi;
+} cur_info = {0.0, 0.0, 0.0};
 
 // Phi angle radians per single tick
 // (RADIUS/BASELINE) phi radians/wheel radians 
@@ -39,19 +39,21 @@ struct __attribute__((__packed__)) robot_info
 // 1/GEAR_RATIO wheel rotations/encoder rotation
 // 1/TICKS_PER_ROT encoder rotation/tick
 // So phi radians / tick = ((RADIUS/BASELINE) phi radians/wheel radians)*(2pi wheel radians/wheel rotation)*(1/GEAR_RATIO wheel rotations/encoder rotation)*(1/TICKS_PER_ROT encoder rotation/tick)
-const float phi_radians_per_tick = (RADIUS / BASELINE) * (2.0f * PI) * (1.0f / GEAR_RATIO) * (1.0f / TICKS_PER_ROT);
+const double phi_radians_per_tick = (RADIUS / BASELINE) * (PI) * (1.0f / GEAR_RATIO) * (1.0f / TICKS_PER_ROT);
 
 // Constant delta_x and delta_y values based on phi_radians_per_tick used by ISRs
-const float delta_x = (BASELINE / 2.0) * sin(phi_radians_per_tick); // Calculate local delta x based on local phi
-const float delta_y = (BASELINE / 2.0) - ((BASELINE / 2.0) * cos(phi_radians_per_tick)); // Calculate local delta y based on local phi
+const double delta_x = (BASELINE / 2.0) * sin(phi_radians_per_tick); // Calculate local delta x based on local phi
+const double delta_y = (BASELINE / 2.0) - ((BASELINE / 2.0) * cos(phi_radians_per_tick)); // Calculate local delta y based on local phi
 
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 int status = WL_IDLE_STATUS;     // the WiFi radio's status
 
-unsigned int localPort = 5005;      // local port to listen on
+unsigned int localPort = 4242;      // local port to listen on
 
 unsigned int right_speed, left_speed;
+
+int packetSize;
 
 WiFiUDP Udp;
 
@@ -61,8 +63,8 @@ void setup()
   pinMode(MOTOR_RIGHT_B, OUTPUT);
   pinMode(MOTOR_LEFT_A, OUTPUT);
   pinMode(MOTOR_LEFT_B, OUTPUT);
-  pinMode(IR_RIGHT, INPUT);
-  pinMode(IR_LEFT, INPUT);
+  pinMode(IR_RIGHT, INPUT_PULLUP);
+  pinMode(IR_LEFT, INPUT_PULLUP);
   analogWrite(MOTOR_RIGHT_A, 0);
   analogWrite(MOTOR_RIGHT_B, 0);
   analogWrite(MOTOR_LEFT_A, 0);
@@ -90,32 +92,32 @@ void setup()
   Serial.println("\nStarting connection to server...");
   // if you get a connection, report back via serial:
   Udp.begin(localPort);
+
+  cur_info.x = 0.0;
+  cur_info.y = 0.0;
+  cur_info.phi = 0.0;
 }
 
 void loop()
 {
-  int packetSize = Udp.parsePacket();
-
-  if (packetSize)
+  if (packetSize = Udp.parsePacket())
   {
     Udp.read((char*)(&input_command), sizeof(command));
-    Serial.println(input_command.translational, 5);
-    Serial.println(input_command.rotational, 5);
-    Serial.println(input_command.mode);
-
     Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
     Udp.write((char*)(&cur_info));
     Udp.endPacket();
   }
 
-  right_speed = ((2.0f * input_command.translational) + (input_command.rotational * BASELINE)) / (2.0f * RADIUS);
-  left_speed = ((2.0f * input_command.translational) - (input_command.rotational * BASELINE)) / (2.0f * RADIUS);
+  right_speed = ((2.0 * input_command.translational) + (input_command.rotational * BASELINE)) / (2.0 * RADIUS);
+  left_speed = ((2.0 * input_command.translational) - (input_command.rotational * BASELINE)) / (2.0 * RADIUS);
 
   if (right_speed > 255) right_speed = 255;
   if (left_speed > 255) left_speed = 255;
   
   analogWrite(MOTOR_RIGHT_A, right_speed);
   analogWrite(MOTOR_LEFT_A, left_speed);
+
+  delay(20);
 }
 
 void printWiFiData() {
@@ -176,15 +178,19 @@ void printMacAddress(byte mac[])
 // ISR for calculating odometry of right wheel based on IR sensor signal using analytical method
 void analytical_odometry_right()
 { 
+  cur_info.phi *= (PI / 180.0);
   cur_info.phi += phi_radians_per_tick; // Calculate new global phi angle based on ratio between phi radians and ticks
   cur_info.x += (delta_x * cos(cur_info.phi)) + (delta_y * sin(cur_info.phi));  // Update global x based on global phi and local delta x and y
   cur_info.y += (delta_x * sin(cur_info.phi)) + (delta_y * cos(cur_info.phi));  // Update global y based on global phi and local delta x and y
+  cur_info.phi *= (180.0 / PI);
 }
 
 // ISR for calculating odometry of left wheel based on IR sensor signal using analytical method
 void analytical_odometry_left()
 {
+  cur_info.phi *= (PI / 180.0);
   cur_info.phi -= phi_radians_per_tick; // Calculate new global phi angle based on ratio between phi radians and ticks
   cur_info.x += (delta_x * cos(cur_info.phi)) + (delta_y * sin(cur_info.phi));  // Update global x based on global phi and local delta x and y
   cur_info.y -= (delta_x * sin(cur_info.phi)) + (delta_y * cos(cur_info.phi));  // Update global y based on global phi and local delta x and y
+  cur_info.phi *= (180.0 / PI);
 }
